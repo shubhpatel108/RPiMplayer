324e55f40efa18ba175ba5cca2773c37549ffaf0
Shubham Patel <shubhpatel108@gmail.com>
2014-10-13 02:19:54 +0530

    Mplayer now runs as independent process and takes inpt from parent

--- script.py
+++ script.py
@@ -1,1 +1,20 @@
-print "Hello!!"
+import os
+import time
+
+def child():
+	command = "mplayer -ao alsa -slave -input file=/tmp/pipe -quiet " + file_name
+	os.system(command)
+	os._exit(0)  
+
+def parent():
+	newpid = os.fork()
+	if newpid == 0:
+		child()
+	else:
+		while True:
+			cmd = raw_input()
+			exe_cmd = "echo " + cmd + " > /tmp/pipe"
+			os.system(exe_cmd)
+
+file_name = raw_input("Enter path and name of the file to play: ")
+parent()
